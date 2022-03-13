1bbd8002a15735e03c9fb177a0fb2a9d035ee473
scriptvpskita <88125458+scriptvpskita@users.noreply.github.com>
2022-03-14 02:29:47 +0800

    Update salome-ssh.sh

--- ws-epro/salome-ssh.sh
+++ ws-epro/salome-ssh.sh
@@ -14,7 +14,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/ws-epro -salome -listen :443 -ssh 127.0.0.1:22 -vpn 127.0.0.1:442 -ssl 127.0.0.1:777 -default 127.0.0.1:9999 -f config.yml
+ExecStart=/usr/bin/ws-epro -salome -listen :443 -ssh 127.0.0.1:22 -vpn 127.0.0.1:442 -ssl 127.0.0.1:777
 Restart=on-failure
 
 [Install]
